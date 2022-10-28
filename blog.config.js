186232ecda64ee2f781ed52eafb2aa6cc5a07074
jackH0311 <116907726+jackH0311@users.noreply.github.com>
2022-10-29 01:25:12 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,6 +1,6 @@
 // 注: process.env.XX是Vercel的环境变量，配置方式见：https://docs.tangly1024.com/zh/features/personality
 const BLOG = {
-  AUTHOR: '老黄爱搞副业', // 作者
+  AUTHOR: '老就克劳福德业', // 作者
   BIO: '一个普通的干饭人🍚', // 作者简介
   LINK: 'https://tangly1024.com', // 网站地址
   KEYWORDS: 'Notion, 博客', // 网站关键词 英文逗号隔开
